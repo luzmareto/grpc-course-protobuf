5a46ef37bf042d012f217ab5e551610084ad31e7
Luzmareto Rusmadian <900179@hq.bni.co.id>
2025-08-27 10:37:15 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"grp-course-protobuf/pb/user"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type userService struct {
+	user.UnimplementedUserServiceServer
+}
+
+func (us *userService) CreateUser(ctx context.Context, userRequest *user.User) (*user.CreateResponse, error) {
+	log.Println("user is created")
+	return &user.CreateResponse{
+		Message: "user created",
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Fatal("There is error in your net listen ", err)
+	}
+
+	serv := grpc.NewServer()
+
+	user.RegisterUserServiceServer(serv, &userService{})
+
+	if err := serv.Serve(lis); err != nil {
+		log.Fatal("Error running server ", err)
+	}
+}
